828c0754121d28f18ccea8db6894b58510b33adf
vic85821 <vic85821@gmail.com>
2019-05-07 10:57:24 +0800

    add testcase2 and rename the test_order to testcase3

--- Team1/Main.java
+++ Team1/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Application app = new Application();
+		String style = new String("Motif");
 		
 		try {
 			File input = new File(args[0]);
@@ -17,14 +18,18 @@ public class Main {
 			while ((line = reader.readLine()) != null) {
 				String[] tokens = line.split(" ");
 				
-				if ((tokens[0].equals("Window")) || (tokens[0].equals("ScrollBar")) || (tokens[0].equals("Button")))
+				if ((tokens[0].equals("Window")) || (tokens[0].equals("ScrollBar")) || (tokens[0].equals("Button"))) {
 					app.create(tokens[0], tokens[1]);
-				else if (tokens[0].equals("Present"))
+					app.update(style);
+				} else if (tokens[0].equals("Present")) {
 					app.present();
-				else if (tokens[0].equals("PM"))
+				} else if (tokens[0].equals("PM")) {
 					app.update(tokens[0]);
-				else if (tokens[0].equals("Motif"))
+					style = tokens[0];
+				} else if (tokens[0].equals("Motif")) {
 					app.update(tokens[0]);
+					style = tokens[0];
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
